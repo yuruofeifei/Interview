b9dda2b7a802da19897dbd56b1d954e70518b0bb
yuruofeifei <yuruofeifei@gmail.com>
2014-09-12 21:42:57 -0700

    Update ValidPalindrome.cpp

--- leetcode/ValidPalindrome.cpp
+++ leetcode/ValidPalindrome.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
+    bool isValid(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    }
     bool isPalindrome(string s) {
         int start = 0, end = s.size() - 1;
         while (start < end) {
-            char left, right;
-            if (s[start] >= 97 && s[start] <= 122) {
-                left = s[start] - 32;
-            }
-            else if ((s[start] >= 65 && s[start] <= 90) || (s[start] >= 48 && s[start] <= 57)) {
-                left = s[start];
-            }
-            else {
-                start ++;
-                continue;
-            }
-            
-            if (s[end] >= 97 && s[end] <= 122) {
-                right = s[end] - 32;
-            }
-            else if ((s[end] >= 65 && s[end] <= 90) || (s[end] >= 48 && s[end] <= 57)) {
-                right = s[end];
-            }
-            else {
-                end --;
-                continue;
-            }
-            
-            if (left == right) {
-                start ++;
-                end --;
-            }
-            else {
-                return false;
-            }
+            if (!isValid(s[start])) { start ++; continue; }
+            if (!isValid(s[end])) { end --; continue; }
+            if (s[start] != s[end] && abs(s[start] - s[end]) != 32) return false;
+            start ++;
+            end --;
         }
         return true;
     }
