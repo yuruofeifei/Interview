3b9e08945e493cc61105a6883766db5e44646296
yuruofeifei <yuruofeifei@gmail.com>
2014-09-12 19:17:04 -0700

    Update ReverseWordInAString.cpp

--- leetcode/ReverseWordInAString.cpp
+++ leetcode/ReverseWordInAString.cpp
@@ -2,50 +2,16 @@ class Solution {
 public:
     void reverseWords(string &s) {
         reverse(s.begin(), s.end());
-        int start = -1, end = -1;
+        int storeIndex = 0;
         for (int i = 0; i < s.size(); i++) {
             if (s[i] != ' ') {
-                if (start == -1) {
-                    start = i;
-                }
-                end = i;
-            }
-            else {
-                if (start != -1) {
-                    for (int i = start; i <= (start + end) / 2; i++) {
-                        char temp = s[i];
-                        s[i] = s[end + start - i];
-                        s[end + start - i] = temp;
-                    }
-                    start = -1;
-                    end = -1;
-                }
+                if (storeIndex != 0) s[storeIndex++] = ' ';
+                int j = i;
+                while (j < s.size() && s[j] != ' ') { s[storeIndex++] = s[j++]; }
+                reverse(s.begin() + storeIndex - (j - i), s.begin() + storeIndex);
+                i = j;
             }
         }
-        for (int i = start; i <= (start + end) / 2; i++) {
-            char temp = s[i];
-            s[i] = s[end + start - i];
-            s[end + start - i] = temp;
-        }
-        bool leading = true, continous = false;
-        int index = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == ' ') {
-                if (leading) {
-                    continue;
-                }
-                if (continous) {
-                    continue;
-                }
-                continous = true;
-            }
-            else {
-                leading = false;
-                if (continous) s[index++] = ' ';
-                continous = false;
-                s[index++] = s[i];
-            }
-        }
-        s.erase(s.begin() + index, s.end());
+        s.erase(s.begin() + storeIndex, s.end());
     }
 };
