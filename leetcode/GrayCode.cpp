11e5cebfab1b6bdc60bb167843e1c19aa7322247
yuruofeifei <yuruofeifei@gmail.com>
2014-09-23 23:15:56 -0700

    Update GrayCode.cpp

--- leetcode/GrayCode.cpp
+++ leetcode/GrayCode.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
-    void gray(int digit, int n, vector<int> &v) {
-        if (digit == n) {
-            return;
-        }
-        int add = 1 << digit;
-        int size = v.size();
-        for (int i = size - 1; i >= 0; i--) {
-            v.push_back(v[i] + add);
-        }
-        gray(digit + 1, n, v);
-    }
     vector<int> grayCode(int n) {
-        vector<int> v; v.push_back(0); 
-        gray(0, n, v);
-        return v;
-    }
-};
-class Solution {
-public:
-    vector<int> grayCode(int n) {
-        vector<int> v;
-        v.push_back(0);
-        if (n == 0) return v;
-        v.push_back(1);
+        if (n == 0) return {0};
+        vector<int> v = {0, 1};
+        int base = 2;
         for (int i = 1; i < n; i++) {
-            int nn = v.size();
-            int mm = 1 << i;
-            for (int j = 0; j < mm; j++) {
-                v.push_back(nn + v[mm - j - 1]);
+            for (int j = base - 1; j >= 0; j--) {
+                v.push_back(v[j] + base);
             }
+            base *= 2;
         }
         return v;
     }
