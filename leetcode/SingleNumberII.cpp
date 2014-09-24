f765190dd83af853e0bd0b8c90c6d71e3b24a572
yuruofeifei <yuruofeifei@gmail.com>
2014-09-23 18:53:56 -0700

    Update SingleNumberII.cpp

--- leetcode/SingleNumberII.cpp
+++ leetcode/SingleNumberII.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int singleNumber(int A[], int n) {
-        int single = 0;
-        int count[32] = {0};
+        int count[32];
+        for (int i = 0; i < 32; i++) count[i] = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 32; j++) {
-                if ((A[i] >> j) & 1) {
-                    count[j] ++;
-                    if (count[j] == 3) {
-                        single &= ~(1 << j);
-                        count[j] = 0;
-                    }
-                    if (count[j] == 1) {
-                        single |= (1 << j);
-                    }
-                }
+                if (A[i] & (1 << j)) count[j] = (count[j] + 1) % 3;
             }
         }
-        return single;
+        int ret = 0;
+        for (int i = 0; i < 32; i++) if (count[i] == 1) ret |= (1 << i);
+        return ret;
     }
 };
