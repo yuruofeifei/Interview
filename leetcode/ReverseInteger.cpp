04d2f894f5db7c834fa17744c85d475b19626109
yuruofeifei <yuruofeifei@gmail.com>
2014-09-23 17:03:11 -0700

    Update ReverseInteger.cpp

--- leetcode/ReverseInteger.cpp
+++ leetcode/ReverseInteger.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int reverse(int x) {
-        int negtive = 1;
-        if (x == 0) return 0;
-        if (x < 0) { x = -x; negtive = -1;}
-        int numDigits = 0;
-        int temp = x;
-        while (temp != 0) {
-            temp /= 10;
-            numDigits ++;
+        int sign = 1;
+        if (x < 0) {x = -x; sign = -1;}
+        int ret = 0;
+        while (x) {
+            ret = 10 * ret + (x % 10);
+            x /= 10;
         }
-        for (int i = 0; i <= (numDigits - 1) / 2; i++) {
-            int high = x / (int)pow(10, numDigits - i - 1) % 10;
-            int low = x / (int)(pow(10, i)) % 10;
-            x += (low - high) * (int)pow(10, numDigits - i - 1);
-            x += (high - low) * (int)pow(10, i);
-        }
-        return x * negtive;
+        return ret * sign;
     }
 };
