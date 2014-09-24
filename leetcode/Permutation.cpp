9d93c9d071856b67049d30550b843e7d9b4d8d7f
yuruofeifei <yuruofeifei@gmail.com>
2014-09-24 02:13:28 -0700

    Update Permutation.cpp

--- leetcode/Permutation.cpp
+++ leetcode/Permutation.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<vector<int> > permute(vector<int> &num) {
-        vector<vector<int>> *ret = new vector<vector<int>>();
-        if (num.empty()) return *ret;
-        vector<int> v = {num[0]};
-        ret->push_back(v);
-        vector<vector<int>> *p = new vector<vector<int>>();
-        for(int i = 1; i < num.size(); i++) {
-            for (int j = 0; j < ret->size(); j++) {
-                for (int k = 0; k <= i; k++) {
-                    vector<int> temp = ret->at(j);
+        vector<vector<int>> v1, v2;
+        v1.push_back({});
+        vector<vector<int>> &ret = v1, &v = v2;
+        for (int i = 0; i < num.size(); i++) {
+            for (int j = 0; j < ret.size(); j++) {
+                for (int k = 0; k <= ret[j].size(); k++) {
+                    vector<int> temp = ret[j];
                     temp.insert(temp.begin() + k, num[i]);
-                    p->push_back(temp);
+                    v.push_back(temp);
                 }
             }
-            vector<vector<int>> *mid = ret;
-            ret = p;
-            p = mid;
-            p->clear();
+            vector<vector<int>> &mid = ret;
+            ret = v;
+            v = ret;
+            v.clear();
         }
-        return *ret;
+        return ret;
     }
 };
