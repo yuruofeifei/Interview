c809cbe166e41c9d82c35da92bcf0efe080476c5
yuruofeifei <yuruofeifei@gmail.com>
2014-09-24 21:53:25 -0700

    Update LengthOfLastWord.cpp

--- leetcode/LengthOfLastWord.cpp
+++ leetcode/LengthOfLastWord.cpp
@@ -3,19 +3,12 @@ public:
     int lengthOfLastWord(const char *s) {
         int start = -1;
         int length = 0;
-        for (int i = 0; *s != '\0'; i++, s++) {
-            if (*s != ' ') {
-                if (start == -1) {
-                    start = i;
-                    length = 1;
-                }
-                else {
-                    length = i - start + 1;
-                }
+        for (int i = 0; s[i]; i++) {
+            if (s[i] != ' ') {
+                if (start == -1) start = i;
+                length = i - start + 1;
             }
-            else {
-                start = -1;
-            }
+            else start = -1;
         }
         return length;
     }
