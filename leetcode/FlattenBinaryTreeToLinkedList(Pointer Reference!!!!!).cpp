b13709e51e42e63c18b27839b11edfbd0f6ac264
yuruofeifei <yuruofeifei@gmail.com>
2014-09-24 21:53:41 -0700

    Update FlattenBinaryTreeToLinkedList(Pointer Reference!!!!!).cpp

--- leetcode/FlattenBinaryTreeToLinkedList(Pointer Reference!!!!!).cpp
+++ leetcode/FlattenBinaryTreeToLinkedList(Pointer Reference!!!!!).cpp
@@ -9,44 +9,16 @@
  */
 class Solution {
 public:
-    void connect(TreeNode *root, TreeNode *&l, TreeNode *&r) {
-        if (!root) {
-            l = NULL; r = NULL;
-            return ;
-        }
-        TreeNode *ll = NULL, *lr = NULL, *rl = NULL, *rr = NULL;
-        connect(root->left, ll, lr);
-        connect(root->right, rl, rr);
-        root->left = NULL;
-        l = root; r = root;
-        if (ll) { root->right = ll; lr->right = rl;  r = lr; }
-        if (rl) { r = rr; }
-    }
     void flatten(TreeNode *root) {
-        TreeNode *l, *r;
-        return connect(root, l, r);
-    }
-};
-
-class Solution {
-public:
-    void build(TreeNode *root, TreeNode *&tmp)
-    {
-        if(root)
-        {
-            build(root->right, tmp);
-            build(root->left, tmp);
-            
-            root->right=tmp;
-            tmp=root;
-            root->left=NULL;
+        while ( root ) {
+            if ( root->left ) {
+                TreeNode *ptr = root->left;
+                while ( ptr->right ) ptr = ptr->right;
+                ptr->right = root->right;
+                root->right = root->left;
+                root->left = NULL;
+            }
+            root = root->right;
         }
-    }
-    void flatten(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        TreeNode *tmp=NULL;
-        build(root, tmp);
-        
     }
 };
